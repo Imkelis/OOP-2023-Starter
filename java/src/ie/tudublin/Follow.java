b8064193fe828776a10c9c6a3406e5a385f82263
Imkelis <123948448+Imkelis@users.noreply.github.com>
2023-04-26 17:40:34 +0100

    Struggling to make it work with the follow class, would be easier without it

---
+++ java/src/ie/tudublin/Follow.java
@@ -0,0 +1,14 @@
+package ie.tudublin;
+
+public class Follow {
+
+    int count;
+    String word;
+
+    public Follow(String word) {
+
+        this.word = word;
+        this.count = 1;
+    }
+
+}
--- java/src/ie/tudublin/Word.java
+++ java/src/ie/tudublin/Word.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Word {
 
-    ArrayList<String> words = new ArrayList<String>();
+    ArrayList<Follow> words = new ArrayList<Follow>();
 
     public Word() {
 
@@ -12,13 +12,62 @@ public class Word {
 
     public void addWord(String word) {
 
-        if (words.contains(word)) {
+        int last = words.lastIndexOf(words);
 
-        } else {
-            words.add(word);
+        if (words.isEmpty()) {
+            Follow f = new Follow(word);
+            words.add(f);
         }
-        System.out.println(words);
 
+        for (Follow fs : words) {
+
+            if (fs.word == word) {
+                fs.count++;
+                break;
+            }
+
+            int current = words.indexOf(fs);
+
+            System.out.println(last);
+
+            if (fs.word != word) {
+
+                if (last == current) {
+                    Follow f = new Follow(word);
+                    words.add(f);
+
+                }
+
+                continue;
+            }
+
+        }
     }
 
 }
+
+// for (Follow fs : words) {
+
+// if(fs.word != word){
+// continue;
+// }
+// if(fs.word == word){
+// fs.count++;
+// break;
+// }
+
+// }
+// else {
+// Follow f = new Follow(word);
+// words.add(f);
+// }
+
+// if (words.contains(word)) {
+// int s = words.indexOf(word);
+
+// } else {
+// Follow f = new Follow(word);
+// words.add(f);
+// }
+
+// }
