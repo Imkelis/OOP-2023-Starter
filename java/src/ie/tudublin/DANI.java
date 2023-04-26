62b5c434d5cdf188debc522ccfdc597797f6ba03
Imkelis <123948448+Imkelis@users.noreply.github.com>
2023-04-26 16:43:40 +0100

    Found a way to seperate the words from the lines, and started word class

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -6,24 +6,44 @@ import processing.core.PApplet;
 
 public class DANI extends PApplet {
 
-	
+	String[] line;
+	String[] words;
 
 	public void settings() {
 		size(1000, 1000);
-		//fullScreen(SPAN);
+		// fullScreen(SPAN);
 	}
 
-    String[] sonnet;
+	String[] sonnet;
 
-    public String[] writeSonnet()
-    {
-        return null;
-    }
+	public String[] writeSonnet() {
+		return null;
+	}
 
 	public void setup() {
 		colorMode(HSB);
 
-       
+	}
+
+	public void loadFile() {
+
+		line = loadStrings("small.txt");
+
+		for (String s : line) {
+
+			s = s.toLowerCase();
+
+			words = split(s, ' ');
+
+			for (String word : words) {
+				word = word.replaceAll("[^\\w\\s]", "");
+
+				System.out.println(word);
+
+			}
+
+		}
+
 	}
 
 	public void keyPressed() {
@@ -32,13 +52,13 @@ public class DANI extends PApplet {
 
 	float off = 0;
 
-	public void draw() 
-    {
+	public void draw() {
 		background(0);
 		fill(255);
 		noStroke();
 		textSize(20);
-        textAlign(CENTER, CENTER);
-        
+		textAlign(CENTER, CENTER);
+		loadFile();
+
 	}
 }
---
+++ java/src/ie/tudublin/Word.java
@@ -0,0 +1,23 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+
+public class Word {
+
+    ArrayList<String> words = new ArrayList<String>();
+
+    public Word() {
+
+    }
+
+    public void addWord(String word) {
+
+        if (words.contains(word)) {
+
+        } else {
+            words.add(word);
+        }
+
+    }
+
+}
